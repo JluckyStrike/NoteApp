c634fb603b37f73a00fa8787de99080def80fa0c
JluckyStrike <it.fun@mail.ru>
2021-12-08 21:27:36 +0500

    Добавил боковое меню

--- app/src/main/java/com/itfun/noteapp/MainActivity.java
+++ app/src/main/java/com/itfun/noteapp/MainActivity.java
@@ -1,19 +1,30 @@
 package com.itfun.noteapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentResultListener;
 
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.itfun.noteapp.detail.NoteDetailsActivity;
 import com.itfun.noteapp.detail.NoteDetailsFragment;
+import com.itfun.noteapp.nav_drawer.OptionsFragment;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
     private Note selectedNote;
     private static final String ARG_NOTE = "ARG_NOTE";
@@ -22,6 +33,14 @@ public class MainActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_app_open, R.string.nav_app_close);
+
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
 
         if (savedInstanceState != null && savedInstanceState.containsKey(ARG_NOTE)) {
             selectedNote = savedInstanceState.getParcelable(ARG_NOTE);
@@ -32,20 +51,20 @@ public class MainActivity extends AppCompatActivity  {
         }
 
         getSupportFragmentManager()
-        .setFragmentResultListener(NotesListFragment.RESULT_KEY, this, new FragmentResultListener() {
-            @Override
-            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
-                selectedNote = result.getParcelable(NotesListFragment.ARG_NOTE);
+                .setFragmentResultListener(NotesListFragment.RESULT_KEY, this, new FragmentResultListener() {
+                    @Override
+                    public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
+                        selectedNote = result.getParcelable(NotesListFragment.ARG_NOTE);
 
-                if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                    showDetails();
-                } else {
-                    Intent intent = new Intent(MainActivity.this, NoteDetailsActivity.class);
-                    intent.putExtra(NoteDetailsActivity.EXTRA_NOTE, selectedNote);
-                    startActivity(intent);
-                }
-            }
-        });
+                        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                            showDetails();
+                        } else {
+                            Intent intent = new Intent(MainActivity.this, NoteDetailsActivity.class);
+                            intent.putExtra(NoteDetailsActivity.EXTRA_NOTE, selectedNote);
+                            startActivity(intent);
+                        }
+                    }
+                });
 
         findViewById(R.id.about_apps).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,6 +85,24 @@ public class MainActivity extends AppCompatActivity  {
                         .commit();
             }
         });
+
+        NavigationView navigationView = findViewById(R.id.nav_drawer_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_options:
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fragment_container, new OptionsFragment())
+                                .commit();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
@@ -82,5 +119,25 @@ public class MainActivity extends AppCompatActivity  {
                 .beginTransaction()
                 .replace(R.id.details_container, NoteDetailsFragment.newInstance(selectedNote))
                 .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                Toast.makeText(this, "Add", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_search:
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/itfun/noteapp/detail/NoteDetailsFragment.java
+++ app/src/main/java/com/itfun/noteapp/detail/NoteDetailsFragment.java
@@ -1,18 +1,31 @@
 package com.itfun.noteapp.detail;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentResultListener;
 
 import com.itfun.noteapp.Note;
+import com.itfun.noteapp.NotesListFragment;
 import com.itfun.noteapp.R;
 
 public class NoteDetailsFragment extends Fragment {
     private static final String ARG_NOTE = "ARG_NOTE";
+    public static final String KEY_RESULT = "CityDetailsFragment_KEY_RESULT";
+
+    TextView noteTitle;
+    TextView descTitle;
+    TextView dateTitle;
+    ImageView noteImage;
 
     public static NoteDetailsFragment newInstance(Note note){
         NoteDetailsFragment fragment = new NoteDetailsFragment();
@@ -33,13 +46,23 @@ public class NoteDetailsFragment extends Fragment {
 
         Note note = requireArguments().getParcelable(ARG_NOTE);
 
-        TextView noteTitle = view.findViewById(R.id.notes_title);
+        noteTitle = view.findViewById(R.id.notes_title);
         noteTitle.setText(note.getTitle());
 
-        TextView descTitle = view.findViewById(R.id.notes_desc);
+        descTitle = view.findViewById(R.id.notes_desc);
         descTitle.setText(note.getDescription());
 
-        TextView dateTitle = view.findViewById(R.id.notes_date);
+        dateTitle = view.findViewById(R.id.notes_date);
         dateTitle.setText(note.getDate());
+
+        noteImage = view.findViewById(R.id.note_image);
+        noteImage.setImageResource(note.getImage());
     }
+
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
 }
