4bf1ef98943602cafa6df075772765d2f943526d
JluckyStrike <it.fun@mail.ru>
2021-12-12 00:26:43 +0500

    Добовил сообщение в приложение

--- app/src/main/java/com/itfun/noteapp/MainActivity.java
+++ app/src/main/java/com/itfun/noteapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.itfun.noteapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -10,6 +11,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentResultListener;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -99,6 +101,26 @@ public class MainActivity extends AppCompatActivity {
                                 .commit();
                         drawerLayout.closeDrawer(GravityCompat.START);
                         return true;
+                    case R.id.action_close_app:
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setTitle(R.string.already_quit)
+                                .setNegativeButton(R.string.str_no, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        Toast.makeText(getApplicationContext(), R.string.welcome_back, Toast.LENGTH_SHORT).show();
+                                        drawerLayout.closeDrawer(GravityCompat.START);
+                                    }
+                                })
+                                .setPositiveButton(R.string.str_yes, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        Toast.makeText(getApplicationContext(), R.string.quit_app, Toast.LENGTH_SHORT).show();
+                                        System.exit(0);
+                                    }
+                                }).show();
+
+                        return true;
+
                 }
                 return false;
             }
